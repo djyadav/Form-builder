4c79af08a24000585033cc9bceb4f048262e02f7
Deepak Kumar <deepak@supply.ai>
2020-12-04 18:09:17 +0530

    Added some margin on the switcher

--- src/views/Home/index.js
+++ src/views/Home/index.js
@@ -3,10 +3,20 @@ import Form from "./../../components/Form";
 
 import { Route } from "react-router-dom";
 import TemplateSwitcher from "./../../components/TemplateSwitcher";
-
+import {
+  Row,
+  Col,
+} from "reactstrap";
 class Home extends Component {
   render() {
-    return <TemplateSwitcher />;
+
+    return (
+      <Row className="mt-5 justify-content-center">
+        <Col lg="6">
+          <TemplateSwitcher />
+        </Col>
+      </Row>
+    )
   }
 }
 
